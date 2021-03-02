835102811e8a70dd20ddf1aab1a24317bffade5c
= <manikhweschool@gmail.com>
2021-03-02 05:18:06 +0200

    Empty Commit

---
+++ src/main/java/com/manikhwe/mycarismyreflection/MycarismyreflectionApplication.java
@@ -0,0 +1,13 @@
+package com.manikhwe.mycarismyreflection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycarismyreflectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycarismyreflectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manikhwe/mycarismyreflection/MycarismyreflectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manikhwe.mycarismyreflection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycarismyreflectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
